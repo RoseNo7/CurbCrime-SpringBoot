9b662bd0b28523a91ab95d8aeb76a7e2ef660195
RoseNo7 <7Roseno@gmail.com>
2024-03-14 22:45:04 +0900

    서비스 예외 처리

--- src/main/java/com/roseno/curbcrime/handler/GlobalExceptionHandler.java
+++ src/main/java/com/roseno/curbcrime/handler/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.roseno.curbcrime.handler;
 
 import com.roseno.curbcrime.dto.api.ApiErrorResponse;
 import com.roseno.curbcrime.dto.api.ApiResult;
+import com.roseno.curbcrime.exception.ServiceException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,5 +33,21 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
 
         return ResponseEntity.badRequest().body(apiErrorResponse);
+    }
+
+    /**
+     * 서비스 예외 처리
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ApiErrorResponse> handleServiceException(ServiceException ex) {
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .status(ApiResult.ERROR.status())
+                .message(ex.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiErrorResponse);
     }
 }
