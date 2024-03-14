805b53135ca2f2a61d443e5c90a8a41b321d433e
RoseNo7 <7Roseno@gmail.com>
2024-03-14 20:05:13 +0900

    Initial project

---
+++ src/main/java/com/roseno/curbcrime/CurbcrimeApplication.java
@@ -0,0 +1,13 @@
+package com.roseno.curbcrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurbcrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurbcrimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roseno/curbcrime/CurbcrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roseno.curbcrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurbcrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
